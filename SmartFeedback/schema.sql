722a35fdd2fe73c154f372c96f23edbdbf2d5f22
Merugu Naveen <naveenmerugu959@gmail.com>
2025-11-08 22:42:52 +0530

    Updated

--- SmartFeedback/schema.sql
+++ SmartFeedback/schema.sql
@@ -1,16 +1,14 @@
--- -- & "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p
-
 CREATE TABLE UserDetails(
     id VARCHAR(50) PRIMARY KEY,
-    userName VARCHAR(50),
+    userName VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE Feedback(
     feedbackId VARCHAR(50) PRIMARY KEY,
     userId VARCHAR(50),
-    message TEXT,
+    message TEXT NOT NULL,
     sentiment VARCHAR(50),
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(userId) REFERENCES UserDetails(id)
@@ -30,5 +28,5 @@ CREATE TABLE admin(
     id int AUTO_INCREMENT PRIMARY KEY,
     adminName VARCHAR(60),
     email VARCHAR(60),
-    password VARCHAR(60)
+    password VARCHAR(60) NOT NULL
 );
